58d3fedcc21987fe366ef07610b0ef8981b11fd7
Mher19971997 <mher.melqonyan.mher123@gmail.com>
2024-06-24 11:24:14 +0400

    changes

--- src/components/OnBoarding/OnBoarding.tsx
+++ src/components/OnBoarding/OnBoarding.tsx
@@ -36,7 +36,7 @@ const OnBoarding = () => {
   return (
     <>
       {
-        !auth().currentUser ?
+        auth().currentUser ?
           <LinearGradient colors={['#485661', '#090A0A']} style={{ width: '100%', height: '100%' }}>
             <LoadingScreen loading={loading} setLoading={setLoading} />
           </LinearGradient>
