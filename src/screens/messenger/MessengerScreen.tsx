209a9742789a8a5322a2be27dbda211e3181ba1f
Mher19971997 <mher.melqonyan.mher123@gmail.com>
2024-06-28 12:03:42 +0400

    messenger some screens

--- src/screens/messenger/MessengerScreen.tsx
+++ src/screens/messenger/MessengerScreen.tsx
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import RN from '../../components/RN';
 import LinearContainer from '../../components/LinearContainer/LinearContainer';
 import HeaderContent from '../../components/HeaderContent/HeaderContent';
 import { Images } from '../../assets';
 import StartBtn from '../../components/StopStartBtn/StopStartBtn';
-import { useNavigation } from '@react-navigation/native';
-import { windowHeight } from '../../utils/styles';
+import { useIsFocused, useNavigation, useRoute } from '@react-navigation/native';
+import { windowHeight, windowWidth } from '../../utils/styles';
 import { APP_ROUTES } from '../../navigation/routes';
 import { Text } from 'react-native';
+import MessageItem from './components/MessageItem';
+import { formatDateTime, secondsToHMS } from '../../helper/helper';
+import useRootStore from '../../hooks/useRootStore';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../types/navigation';
 
+type NavigationProp = StackNavigationProp<RootStackParamList, APP_ROUTES.DIALOG_SCREEN>;
 const MessengerScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<NavigationProp>();
+  const isFocused = useIsFocused();
+
+
+  const {
+    userData,
+    getAllUsers
+  } = useRootStore().messangerStore;
+
+  useEffect(() => {
+    getAllUsers();
+  }, [isFocused]);
+
+
+  const renderItems = useCallback(() => {
+    return userData.map((item, index) => {
+      return (
+        <MessageItem
+          onNavigate={() =>
+            navigation.navigate(APP_ROUTES.DIALOG_SCREEN, {
+              uid: item.uid,
+              name: item.name,
+              avatar: item.avatar
+            })
+          }
+          key={index}
+          avatar={item.avatar}
+          name={item.name}
+          description={
+            ' Hi! Please call me at 16...'
+          }
+          time={'02:30'}
+        />
+      );
+    });
+  }, [userData]);
 
   return (
     <LinearContainer
@@ -19,18 +60,24 @@ const MessengerScreen = () => {
           <HeaderContent
             leftItem={<Images.Svg.btsRightLinear />}
             rightItem={
-              <Images.Svg.searchButton width={39} height={39} onPress={() => navigation.navigate(APP_ROUTES.SEARCH_CONTACT as never)}/>
-          }
+              <Images.Svg.searchButton width={39} height={39} onPress={() => navigation.navigate(APP_ROUTES.SEARCH_CONTACT as never)} />
+            }
             title="Messenger"
           />
           <RN.View style={styles.content}>
-            <Text style={styles.text}>There are no dialogues</Text>
-            <RN.ScrollView
-              showsHorizontalScrollIndicator={false}
-              showsVerticalScrollIndicator={false}>
-              {/* render chats */}
-            </RN.ScrollView>
-            <RN.View>
+            {userData.length == 0 ?
+              <RN.View style={styles.center}>
+                <Text style={styles.text}>There are no dialogues</Text>
+              </RN.View>
+              :
+              <RN.ScrollView
+                style={styles.flatList}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}>
+                {renderItems()}
+              </RN.ScrollView>
+            }
+            <RN.View style={{ position: 'absolute', display: 'flex', width: windowWidth - 12, bottom: 0, justifyContent: 'center', alignItems: 'center', }}>
               <StartBtn
                 elWidth={55}
                 subWidth={75}
@@ -54,23 +101,29 @@ const styles = RN.StyleSheet.create({
   container: {
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
+    // alignItems: 'center',
     paddingHorizontal: 5,
   },
   flatList: {
-    height: windowHeight - windowHeight / 2.6,
+    height: '100%',
   },
   content: {
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: 'red',
+    // alignItems: 'center',
+    // justifyContent: 'center',
     // justifyContent: 'space-between',
-    height: windowHeight - windowHeight / 3.5,
+    height: windowHeight - windowHeight / 4
   },
   text: {
     fontSize: 20,
     color: 'white',
-    position: 'absolute',
-    textAlign: 'center',
   },
+  center: {
+    display: 'flex',
+    height: '90%',
+    width: windowWidth,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
--- src/screens/messenger/components/CustomActions.tsx
+++ src/screens/messenger/components/CustomActions.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { Composer } from 'react-native-gifted-chat';
+import { Composer, InputToolbar } from 'react-native-gifted-chat';
 import { launchImageLibrary } from 'react-native-image-picker';
 import AudioRecorderPlayer from 'react-native-audio-recorder-player';
 import { Images } from '../../../assets';
+import RN from '../../../components/RN';
+import LinearGradient from 'react-native-linear-gradient';
 
 const audioRecorderPlayer = new AudioRecorderPlayer();
 
 const CustomComposer = (props) => {
-    const { text, onTextChanged, composerHeight } = props;
+    const { text, onTextChanged, composerHeight, onSend } = props;
 
     const handlePickImage = () => {
         launchImageLibrary({
@@ -23,12 +25,36 @@ const CustomComposer = (props) => {
                     user: {
                         _id: 1,
                     },
-                    image: response.assets[0].uri,
+                    image: response.assets[0].uri, // Include image URI in the message
+                    fileName: response.assets[0].fileName, // Add fileName to the message
+                    fileSize: (response.assets[0].fileSize / (1024 * 1024)).toFixed(1) + ' MB', // Add fileSize to the message
                 };
-                props.onSend([newMessage]);
+
+                console.log('newMessagenewMessage', newMessage)
+                props.onSend([newMessage]); // Send the new message to the GiftedChat component
             }
         });
     };
+    // const handlePickImage = () => {
+    //     launchImageLibrary({
+    //         mediaType: 'photo',
+    //     }, (response) => {
+    //         if (response.didCancel || response.error) {
+    //             console.log('User cancelled image picker or there was an error');
+    //         } else {
+    //             const newMessage = {
+    //                 _id: Math.random().toString(36).substring(7),
+    //                 createdAt: new Date(),
+    //                 user: {
+    //                     _id: 1,
+    //                 },
+    //                 image: response.assets[0].uri, // Include image URI in the message
+    //             };
+    //             props.onSend([newMessage]); // Send the new message to the GiftedChat component
+    //         }
+    //     });
+    // };
+
 
     const handleRecordVoice = async () => {
         if (props.recording) {
@@ -51,21 +77,55 @@ const CustomComposer = (props) => {
         }
     };
 
+    const handleSendText = () => {
+        if (text.trim()) {
+            const newMessage = {
+                _id: Math.random().toString(36).substring(7),
+                createdAt: new Date(),
+                text,
+                user: {
+                    _id: 1,
+                },
+            };
+            onSend([newMessage]);
+            onTextChanged('');
+        }
+    };
+
     return (
         <View style={styles.composerContainer}>
-            <TouchableOpacity onPress={handlePickImage}>
-                <Images.Svg.imageMessage />
-            </TouchableOpacity>
+        <TouchableOpacity onPress={handlePickImage} style={{paddingTop: 8}}>
+            <Images.Svg.imageMessage />
+        </TouchableOpacity>
+        <View style={styles.composerWrapper}>
+                <Composer
+                    {...props}
+                    text={text}
+                    placeholder='Message'
+                    placeholderTextColor='#636366'
+                    onTextChanged={onTextChanged}
+                    textInputStyle={styles.textInput}
+                    composerHeight={composerHeight}
+                />
+                <TouchableOpacity onPress={handleSendText}>
+                    <RN.Text style={{ color: 'white' }}>Send</RN.Text>
+                </TouchableOpacity>
+            </View>
             {/* <Composer
                 {...props}
                 text={text}
+                placeholder='Message'
+                placeholderTextColor='#636366'
                 onTextChanged={onTextChanged}
                 textInputStyle={styles.textInput}
                 composerHeight={composerHeight}
             /> */}
-             <TouchableOpacity onPress={handleRecordVoice}>
+             <TouchableOpacity onPress={handleRecordVoice} style={{paddingTop: 8}}>
                  <Images.Svg.voiceMessage />
              </TouchableOpacity>
+            {/* <TouchableOpacity onPress={handleSendText}>
+                <RN.Text style={{color: 'white'}}>Send</RN.Text>
+            </TouchableOpacity> */}
          </View>
     );
 };
@@ -73,18 +133,39 @@ const CustomComposer = (props) => {
 const styles = StyleSheet.create({
     composerContainer: {
         flexDirection: 'row',
-        alignItems: 'center',
+        alignItems: 'flex-start',
         paddingHorizontal: 10,
-        paddingVertical: 5,
-        // backgroundColor: 'black',
+        paddingVertical: 12,
+        backgroundColor: '#1C1C1D',
+        gap: 4,
+        // backgroundColor: 'red',
         // borderColor: 'black',
     },
-    textInput: {
+    composerWrapper: {
         flex: 1,
-        padding: 10,
-        backgroundColor: '#f2f2f2',
-        borderRadius: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
     },
+    inputToolbar: {
+        borderTopWidth: 1,
+        borderTopColor: '#e0e0e0',
+      },
+      toolbarContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      textInput: {
+        flex: 1,
+        color: '#636366',
+        borderColor: '#3A3A3C',
+        backgroundColor:'#060606',
+        borderWidth: 1,
+        minHeight: 57,
+        borderRadius: 18,
+        paddingHorizontal: 10,
+        textAlignVertical: 'top',
+        textAlign: 'left',
+      },
 });
 
 export default CustomComposer;
